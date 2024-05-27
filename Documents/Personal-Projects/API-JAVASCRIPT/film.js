60400fa4683eef14aae42c0e051a23922299f351
yusuf sokunbi <adeolaoo87@gmail.com>
2024-05-27 14:45:29 +0100

    remove key

---
+++ Documents/Personal-Projects/API-JAVASCRIPT/film.js
@@ -0,0 +1,44 @@
+// const movie = 'Film'
+// console.log(movie)
+
+// // //  Copy to Clipboard
+// // async function logMovies() {
+// //   const response = await fetch("https://api.example.com/data");
+// //   .then( response => await response.json();
+// //   console.log(movies);
+// // }
+
+// // logMovies();
+
+// const apiUrl = 'https://api.goal.com/';
+// // console.log(apiUrl)
+// // Make a GET request using the Fetch API
+// fetch(apiUrl)
+//   .then(response => {
+//     if (!response.ok) {
+//       throw new Error('Network response was not ok');
+//     }
+//     return response.json();
+//   })
+//   .then(userData => {
+//     // Process the retrieved user data
+//     console.log('User Data:', userData);
+//   })
+//   .catch(error => {
+//     console.error('Error:', error);
+//   });
+
+// function footballNews(){
+//   try{
+
+//   }catch(error){
+// console.error("Error fetching random news", error)
+//   return []
+// }
+  
+// }
+
+
+
+ let Hikmah = "girl"
+const result = undefined || console.log(Hikmah)
